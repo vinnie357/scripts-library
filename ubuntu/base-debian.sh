d9b0955b8fca8b298c2225dd5c03b2698f3e9540
Vinnie Mazza <vinnie.mazza@gmail.com>
2022-04-22 11:26:01 -0400

    Update base-debian.sh

--- ubuntu/base-debian.sh
+++ ubuntu/base-debian.sh
@@ -8,12 +8,7 @@ if [ "$(id -u)" -ne 0 ]; then
 fi
 
 # run update
-if [ ! -d "/var/lib/apt/lists" ] || [ "$(ls /var/lib/apt/lists/ | wc -l)" = "0" ]; then
-    echo "Running apt-get update..."
-    apt-get update
-else
-    echo "Skipping apt-get update."
-fi
+apt-get update
 # Ensure apt is in non-interactive to avoid prompts
 export DEBIAN_FRONTEND=noninteractive
 # install base
