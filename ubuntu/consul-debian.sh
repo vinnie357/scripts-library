e230c92140ddfafc50c2884c4c83dc2e94f167af
Vinnie Mazza <vinnie.mazza@gmail.com>
2022-05-10 11:16:05 -0400

    Create consul-debian.sh

---
+++ ubuntu/consul-debian.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "=====install consul====="
+set -e
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+# install nomad
+curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
+apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+apt-get update && sudo apt-get -y install consul
+echo "===== consul done ====="
