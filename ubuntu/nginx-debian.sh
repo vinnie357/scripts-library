3c83ad291241e327c47807ce127eadb44d5e83e1
Vinnie Mazza <vinnie.mazza@gmail.com>
2022-05-12 13:24:11 -0400

    Update nginx-debian.sh

--- ubuntu/nginx-debian.sh
+++ ubuntu/nginx-debian.sh
@@ -7,8 +7,8 @@ if [ "$(id -u)" -ne 0 ]; then
     echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
     exit 1
 fi
-
-apt install nginx -y
+export DEBIAN_FRONTEND=noninteractive
+apt-get install nginx -y
 
 
 echo "=====finished nginx====="
