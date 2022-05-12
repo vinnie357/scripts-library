6a7d12a2d968f706a9366e4d5abd4eaee6a8e01e
Vinnie Mazza <vinnie.mazza@gmail.com>
2022-05-12 13:44:48 -0400

    Create docker-ce-debian.sh

---
+++ ubuntu/docker-ce-debian.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "=====install docker-ce====="
+set -e
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+export DEBIAN_FRONTEND=noninteractive
+apt update && apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update && apt-get install -y \
+docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+echo "==== docker-ce done ===="
