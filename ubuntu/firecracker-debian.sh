fd40323740c2dd41288a4e336890f5279df61e8d
Vinnie Mazza <vinnie.mazza@gmail.com>
2022-05-16 18:29:35 -0400

    Update firecracker-debian.sh
    snakes and sparklers

--- ubuntu/firecracker-debian.sh
+++ ubuntu/firecracker-debian.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "=====install firecracker====="
+
+DEFAULT_FC_PATH=${1:-"/opt/firecracker"}
+
 set -e
 
 if [ "$(id -u)" -ne 0 ]; then
@@ -7,13 +10,17 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
+mkdir -p "${DEFAULT_FC_PATH}/"{data,config,snakesandsparklers}
+
 # install firecracker
 release_url="https://github.com/firecracker-microvm/firecracker/releases"
 latest=$(basename $(curl -fsSLI -o /dev/null -w  %{url_effective} ${release_url}/latest))
 arch=`uname -m`
 curl -L ${release_url}/download/${latest}/firecracker-${latest}-${arch}.tgz \
-| tar -xz
-mv release-${latest}-$(uname -m) firecracker
-cp firecracker/firecracker-${latest}-$(uname -m) /usr/bin/firecracker
+| tar -xz -C "${DEFAULT_FC_PATH}/"
+mv ${DEFAULT_FC_PATH}/release-${latest}-$(uname -m) ${DEFAULT_FC_PATH}/bin
+cp ${DEFAULT_FC_PATH}/bin/firecracker-${latest}-$(uname -m) /usr/bin/firecracker
+cp ${DEFAULT_FC_PATH}/bin/jailer-${latest}-$(uname -m) /usr/bin/jailer
 firecracker --version
+jailer --version
 echo "==== firecracker done ===="
