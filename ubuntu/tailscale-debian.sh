ee84a5bc14f88a5a82430a9a0e183ffc27edc62e
Vinnie Mazza <vinnie.mazza@gmail.com>
2022-06-13 21:06:15 -0400

    Create tailscale-debian.sh

---
+++ ubuntu/tailscale-debian.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "=====install tailscale====="
+set -e
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+# Ensure apt is in non-interactive to avoid prompts
+export DEBIAN_FRONTEND=noninteractive
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.noarmor.gpg | tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.tailscale-keyring.list | tee /etc/apt/sources.list.d/tailscale.list
+# install tailscale
+apt-get update && apt-get install -y \
+tailscale
+
+echo "==== base done ===="
