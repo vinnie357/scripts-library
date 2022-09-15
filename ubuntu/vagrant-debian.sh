eb2321b175aa943b8d621a3628b542745499bcb4
Vinnie Mazza <vinnie.mazza@gmail.com>
2022-09-15 12:58:14 -0400

    Create vagrant-debian.sh

---
+++ ubuntu/vagrant-debian.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "=====install vagrant====="
+set -e
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+# install vagrant
+export DEBIAN_FRONTEND=noninteractive
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
+apt update && apt install vagrant
+echo "===== vagrant done ====="
+
+
