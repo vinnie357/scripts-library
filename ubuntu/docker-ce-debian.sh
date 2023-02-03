51a39ac2e034bfaa89f85c670e5f6226e39a9624
Vinnie Mazza <vinnie.mazza@gmail.com>
2023-02-03 07:57:46 -0500

    Update docker-ce-debian.sh
    add "docker-compose", so its available in more machines that aren't aware of  plugin style "docker compose" could alias or symlink, but that is more to track

--- ubuntu/docker-ce-debian.sh
+++ ubuntu/docker-ce-debian.sh
@@ -20,6 +20,6 @@ echo \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update && apt-get install -y \
-docker-ce docker-ce-cli containerd.io docker-compose-plugin
+docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose
 
 echo "==== docker-ce done ===="
