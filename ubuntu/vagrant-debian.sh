2fb002723ed60a8ab622412b4cace8ae7352c7ee
Vinnie Mazza <vinnie.mazza@gmail.com>
2023-10-30 09:59:12 -0400

    Update vagrant-debian.sh

--- ubuntu/vagrant-debian.sh
+++ ubuntu/vagrant-debian.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "=====install vagrant====="
+#apt-cache madison vagrant
+VERSION=${1:-${VAGRANT_VERSION:-"2.4.0-1"}}
 set -e
 
 if [ "$(id -u)" -ne 0 ]; then
@@ -11,7 +13,7 @@ fi
 export DEBIAN_FRONTEND=noninteractive
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
-apt update && apt install vagrant
+apt update && apt install vagrant=${VERSION}
 echo "===== vagrant done ====="
 
 
