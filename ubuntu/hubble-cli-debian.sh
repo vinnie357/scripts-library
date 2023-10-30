2e8f3f28f78655cc8d197e57f9fe1b037fc244c8
Vinnie Mazza <vinnie.mazza@gmail.com>
2023-10-30 08:55:29 -0400

    Create hubble-cli-debian.sh

---
+++ ubuntu/hubble-cli-debian.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "=====install hubble-cli====="
+
+set -e
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+export HUBBLE_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/hubble/master/stable.txt)
+curl -L --remote-name-all https://github.com/cilium/hubble/releases/download/$HUBBLE_VERSION/hubble-linux-amd64.tar.gz{,.sha256sum}
+sha256sum --check hubble-linux-amd64.tar.gz.sha256sum
+sudo tar xzvfC hubble-linux-amd64.tar.gz /usr/local/bin
+rm hubble-linux-amd64.tar.gz{,.sha256sum}
+
+# export HUBBLE_SERVER=localhost:4245
+echo "===== hubble-cli done ====="
